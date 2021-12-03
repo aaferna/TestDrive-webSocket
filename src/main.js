5bec3e4636c57249e107b1eb29b6ef98799e837d
Agustin Fernandez <aaferna@outlook.com.ar>
2021-12-02 21:05:50 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-const PORT = 443
+const PORT = process.env.PORT || 8080
 const connectedServer = httpServer.listen(PORT, () => {
     console.log(`Iniciado en ${connectedServer.address().port}`)
 })
